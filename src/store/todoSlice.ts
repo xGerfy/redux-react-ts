77471b42aa3836c4131796b0f6044ffcbe3bde72
Roman Ponomarev <xgerfy@gmail.com>
2024-10-03 13:57:08 +0400

    add store hooks and slice

---
+++ src/store/todoSlice.ts
@@ -0,0 +1,42 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+type Todo = {
+  id: string;
+  title: string;
+  completed: boolean;
+};
+
+type TodosState = {
+  list: Todo[];
+};
+
+const initialState: TodosState = {
+  list: [],
+};
+
+const todoSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo(state, action: PayloadAction<string>) {
+      state.list.push({
+        id: new Date().toISOString(),
+        title: action.payload,
+        completed: false,
+      });
+    },
+    removeTodo(state, action: PayloadAction<string>) {
+      state.list = state.list.filter((todo) => todo.id !== action.payload);
+    },
+    toggleTodoComplete(state, action: PayloadAction<string>) {
+      const toggledTodo = state.list.find((todo) => todo.id === action.payload);
+      if (toggledTodo) {
+        toggledTodo.completed = !toggledTodo.completed;
+      }
+    },
+  },
+});
+
+export const { addTodo, removeTodo, toggleTodoComplete } = todoSlice.actions;
+
+export default todoSlice.reducer;
